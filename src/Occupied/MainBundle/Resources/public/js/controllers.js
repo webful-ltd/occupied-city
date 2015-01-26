0fe7fd1777c476a8909c659b96d32a5ddf536952
Noel Light-Hilary <noel@noellh.com>
2015-01-26 14:24:32 +0200

    Add a sample marker

--- src/Occupied/MainBundle/Resources/public/js/controllers.js
+++ src/Occupied/MainBundle/Resources/public/js/controllers.js
@@ -8,7 +8,17 @@ angular.module('occupied.controllers', [])
                 lat: 51.505,
                 lng: -0.09,
                 zoom: 8
+            },
+            markers: {
+                mainMarker: {
+                    lat: 51,
+                    lng: 0,
+                    focus: true,
+                    message: "Draggable marker",
+                    draggable: true
+                }
             }
+
         });
 
     }])
