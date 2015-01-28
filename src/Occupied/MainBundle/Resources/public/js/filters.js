49296c21d262b5825b88e0e47000f735464e0f9f
Noel Light-Hilary <noel@noellh.com>
2015-01-28 21:59:04 +0200

    Make interpolate filter support {area|...} tags and briefly document what it does

--- src/Occupied/MainBundle/Resources/public/js/filters.js
+++ src/Occupied/MainBundle/Resources/public/js/filters.js
@@ -2,12 +2,23 @@
 
 angular.module('occupied.filters', ['occupied.services'])
 
-    .filter('interpolate', ['$filter', 'basePopulation', function($filter, basePopulation) {
+    /**
+     *  Filter to insert adjusted/ filled-in values for {city}, {population|...}, {area|...}, {year|...}.
+     *
+     *  Population must be an integer with no commas.
+     *  Area must be an int or float with no commas.
+     *  Area will have unit (km^2) appended to the number.
+     */
+    .filter('interpolate', ['$filter', 'basePopulation', 'baseArea', function($filter, basePopulation, baseArea) {
         return function(text, city) {
             text = text.replace(new RegExp('{city}', 'g'), city.name);
 
             text = text.replace(new RegExp('\\{population\\|([0-9]+)\\}', 'g'), function(match, capture) {
                 return $filter('number')(parseInt(capture) * city.population / basePopulation, 0);
+            });
+
+            text = text.replace(new RegExp('\\{area\\|([0-9.]+)\\}', 'g'), function(match, capture) {
+                return $filter('number')(parseFloat(capture) * city.area / baseArea, 1) + ' km²';
             });
 
             text = text.replace(new RegExp('\\{year\\|([0-9]+)\\}', 'g'), function(match, capture) {
