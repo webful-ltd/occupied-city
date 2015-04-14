e20e1d6ad40238f907c788ad65f526999486e406
Noel Light-Hilary <noel@noellh.com>
2015-04-14 12:02:00 +0100

    Document city/country data setup better. Add MultiPolygon support. Add the UK. See #7

--- src/Occupied/MainBundle/Resources/public/js/services.js
+++ src/Occupied/MainBundle/Resources/public/js/services.js
@@ -13,11 +13,29 @@ angular.module('occupied.services', [])
              * Gets data for all cities. N.B. area is km^2 and is for the 'city' / centre.
              * Population estimate dates and sources are in comments.
              *
-             * @returns object  All our data, keyed on city name
+             * @returns object  All our data, keyed on city/country name.
+             *
+             * Keys include:
+             *  `country` - optional bool, assume area is a city if it's missing or false
+             *  `lat` for latitude
+             *  `lng` for longitude
+             *  `area` in km^2
+             *  `coords` is a 3D array in a format ready to be handled as Polygon by Leaflet
+             *  `multicoords` is a 4D array in a format ready to be handled as MultiPolygon
+             *  `population` is an estimate of today's population in the area
+             *
              * @private
              */
             _getAll: function() {
                 return {
+                    'United Kingdom': {
+                        country: true,
+                        lat: 51.5,
+                        lng: -0.116667,
+                        population: 64100000, // 2013 estimate - http://www.ons.gov.uk/ons/rel/pop-estimate/population-estimates-for-uk--england-and-wales--scotland-and-northern-ireland/2013/index.html
+                        area: 243610,
+                        multicoords: [[[[-5.661949,54.554603],[-6.197885,53.867565],[-6.95373,54.073702],[-7.572168,54.059956],[-7.366031,54.595841],[-7.572168,55.131622],[-6.733847,55.17286],[-5.661949,54.554603]]],[[[-3.005005,58.635],[-4.073828,57.553025],[-3.055002,57.690019],[-1.959281,57.6848],[-2.219988,56.870017],[-3.119003,55.973793],[-2.085009,55.909998],[-2.005676,55.804903],[-1.114991,54.624986],[-0.430485,54.464376],[0.184981,53.325014],[0.469977,52.929999],[1.681531,52.73952],[1.559988,52.099998],[1.050562,51.806761],[1.449865,51.289428],[0.550334,50.765739],[-0.787517,50.774989],[-2.489998,50.500019],[-2.956274,50.69688],[-3.617448,50.228356],[-4.542508,50.341837],[-5.245023,49.96],[-5.776567,50.159678],[-4.30999,51.210001],[-3.414851,51.426009],[-3.422719,51.426848],[-4.984367,51.593466],[-5.267296,51.9914],[-4.222347,52.301356],[-4.770013,52.840005],[-4.579999,53.495004],[-3.093831,53.404547],[-3.09208,53.404441],[-2.945009,53.985],[-3.614701,54.600937],[-3.630005,54.615013],[-4.844169,54.790971],[-5.082527,55.061601],[-4.719112,55.508473],[-5.047981,55.783986],[-5.586398,55.311146],[-5.644999,56.275015],[-6.149981,56.78501],[-5.786825,57.818848],[-5.009999,58.630013],[-4.211495,58.550845],[-3.005005,58.635]]]]
+                    },
                     London: {
                         lat: 51.5,
                         lng: -0.116667,
@@ -382,8 +400,21 @@ angular.module('occupied.services', [])
                 var geojson = {};
                 var paths = {};
                 var circleRadius = Math.sqrt(city.area / Math.PI) * 1000; // in m
+                var geometry = false;
 
                 if ('coords' in city) {
+                    geometry = {
+                        "type": "Polygon",
+                        "coordinates": city['coords']
+                    };
+                } else if('multicoords' in city) {
+                    geometry = {
+                        "type": "MultiPolygon",
+                        "coordinates": city['multicoords']
+                    };
+                }
+
+                if (geometry !== false) {
                     geojson = {
                         data: {
                             "type": "FeatureCollection",
@@ -391,10 +422,7 @@ angular.module('occupied.services', [])
                                 {
                                     "type": "Feature",
                                     "properties": {"name": city.name},
-                                    "geometry": {
-                                        "type": "Polygon",
-                                        "coordinates": city['coords']
-                                    }
+                                    "geometry": geometry
                                 }
                             ]
                         },
